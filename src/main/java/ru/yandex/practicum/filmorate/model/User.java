f1599c65355a6e336257ada165ded19bddd61b11
Alexey Zinchenko <zizann86@yandex.ru>
2025-04-11 12:37:49 +0500

    Спринт 11. Итерация 2. Устранение Checkstyle в классе User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -29,7 +29,7 @@ public class User {
 
     @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
-    private Set<Long> friends =  new HashSet<>();;
+    private Set<Long> friends =  new HashSet<>();
 
 
     public String getName() {
